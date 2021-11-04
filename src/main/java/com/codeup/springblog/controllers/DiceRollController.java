a2573ff8e37f13b062afdd0f76773c3f97bc3dc1
afonse03 <amanda.m.fonseca03@gmail.com>
2021-11-03 18:33:41 -0600

    feat: working solution for views exercise

---
+++ src/main/java/com/codeup/springblog/controllers/DiceRollController.java
@@ -0,0 +1,17 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DiceRollController {
+    @GetMapping("/roll-dice")
+    public String diceRoll() {
+        return "roll-dice/roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String diceRollReturn() {
+
+    }
+}
--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -1,4 +1,32 @@
-package com.codeup.springblog.controllers;
-
-public class MathController {
-}
+//package com.codeup.springblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class MathController {
+//    @GetMapping("/add/{}/and/{}")
+//    @ResponseBody
+//    public int add() {
+//
+//    }
+//
+//    @GetMapping("/subtract/{}/from/{}")
+//    @ResponseBody
+//    public int subtract() {
+//
+//    }
+//
+//    @GetMapping("/multiply/{}/and/{}")
+//    @ResponseBody
+//    public int multiply() {
+//
+//    }
+//
+//    @GetMapping("/divide/{}/by/{}")
+//    @ResponseBody
+//    public int divide() {
+//
+//    }
+//}
