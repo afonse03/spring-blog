95ec141ce18a00a921ed8244710bbb16727174d1
afonse03 <amanda.m.fonseca03@gmail.com>
2021-11-09 11:14:04 -0700

    finished controllers exercise

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -1,3 +1,5 @@
+//// DONE todo: This controller should listen for requests for several routes that correspond to basic arithmetic operations and produce the result of the arithmetic.
+//
 //package com.codeup.springblog.controllers;
 //
 //import org.springframework.stereotype.Controller;
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,25 +2,42 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
+
+//    private final PostRepository postRepository;
+//    private final UserRepository userRepository;
+//    private final EmailService emailService;
+//
+//    public PostController(PostRepository postRepository, UserRepository userRepository, EmailService emailService) {
+//        this.postRepository = postRepository;
+//        this.userRepository = userRepository;
+//        this.emailService = emailService;
+//    }
+
     @GetMapping("/posts")
+    @ResponseBody
     public String postsIndex() {
         return "posts index page";
     }
 
     @GetMapping("/posts/{id}")
-    public String individualPost() {
-        return "view an individual post";
+    @ResponseBody
+    public String individualPost(@PathVariable long id) {
+        return "view an individual post" + id;
     }
 
     @GetMapping("/posts/create")
+    @ResponseBody
     public String createPost() {
         return "view the form for creating a post";
     }
 
     @GetMapping("/posts/create")
+    @ResponseBody
     public String newPost() {
         return "create a new post";
     }
