17210cab2efcb8d6a2639a945293bcf1e53b95e4
afonse03 <amanda.m.fonseca03@gmail.com>
2021-11-10 09:44:25 -0700

    setting up for integration tests exercise

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -1,23 +1,23 @@
-package com.codeup.springblog.models;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "users")
-public class User {
-    /* ... */
-
-    public User(User copy) {
-        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
-        email = copy.email;
-        username = copy.username;
-        password = copy.password;
-    }
-
-    public CharSequence getPassword() {
-    }
-
-    public void setPassword(String hash) {
-    }
-}
+//package com.codeup.springblog.models;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "users")
+//public class User {
+//    /* ... */
+//
+//    public User(User copy) {
+//        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+//        email = copy.email;
+//        username = copy.username;
+//        password = copy.password;
+//    }
+//
+//    public CharSequence getPassword() {
+//    }
+//
+//    public void setPassword(String hash) {
+//    }
+//}
--- src/main/java/com/codeup/springblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/springblog/repositories/UserRepository.java
@@ -1,8 +1,8 @@
-package com.codeup.springblog.repositories;
-
-import com.codeup.springblog.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
+//package com.codeup.springblog.repositories;
+//
+//import com.codeup.springblog.models.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    User findByUsername(String username);
+//}
